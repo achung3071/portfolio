5451b86f5a34903609c43db34c2f5c440df11b0f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-13 22:42:19 +0000

    Deploy achung3071/portfolio to github.com/achung3071/portfolio.git:gh-pages

---
+++ precache-manifest.8a9bbe9f5822de7411ad2f341deee1b0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93323bed9f20153ed877053878ca9122",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "973ec0d0aab19fa8d8e0",
+    "url": "/portfolio/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "85aa54acf3a1364dc4fd",
+    "url": "/portfolio/static/css/main.389ed45b.chunk.css"
+  },
+  {
+    "revision": "973ec0d0aab19fa8d8e0",
+    "url": "/portfolio/static/js/2.a06bee93.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.a06bee93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85aa54acf3a1364dc4fd",
+    "url": "/portfolio/static/js/main.39a4a39a.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "1d3697a4e9ee53961a5cbf082502c883",
+    "url": "/portfolio/static/media/andrew_resume_main.1d3697a4.png"
+  },
+  {
+    "revision": "93bc822fcefbb4bfff7f3a54c3320642",
+    "url": "/portfolio/static/media/andrew_resume_main.93bc822f.pdf"
+  },
+  {
+    "revision": "ad770d67c8c7e65b7f5b084e5dcb3c9e",
+    "url": "/portfolio/static/media/booktogether.ad770d67.png"
+  },
+  {
+    "revision": "2e1cc04184c4ae53222508bf73357473",
+    "url": "/portfolio/static/media/find_your_fit.2e1cc041.png"
+  },
+  {
+    "revision": "d078968bc905921e539a6f954e1e2ca1",
+    "url": "/portfolio/static/media/header_background.d078968b.jpg"
+  },
+  {
+    "revision": "8599ad98e6513b577096ed3ceeaa3e39",
+    "url": "/portfolio/static/media/itemshare.8599ad98.png"
+  },
+  {
+    "revision": "b25fa19da036224dc4864ec4b034eca6",
+    "url": "/portfolio/static/media/projects_background.b25fa19d.jpg"
+  },
+  {
+    "revision": "a1648c1cf5b3533f29c9cf7ccdcddb3a",
+    "url": "/portfolio/static/media/taker_assistant.a1648c1c.png"
+  },
+  {
+    "revision": "05bd59c9f6ec148f88cc340d7ae960e3",
+    "url": "/portfolio/static/media/vidplus.05bd59c9.png"
+  }
+]);
